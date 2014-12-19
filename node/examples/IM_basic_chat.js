d0b8fa4389184444f6f671e578016a5df96d7bc4
magnus.lundstedt <magnus.lundstedt@rebtel.com>
2014-12-19 14:38:05 +0100

    Improved Node.js sample app with fallback to create user + better error handling

--- node/examples/IM_basic_chat.js
+++ node/examples/IM_basic_chat.js
@@ -18,6 +18,10 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
+rl.on('SIGINT', function() {
+    quit();
+});
+
 rl.question("Application key: ", function(answer) {
     init(answer);
     var loginObj = {};
@@ -52,27 +56,53 @@ var init = function(appKey) {
 }
 
 var authenticate = function(loginObj) {
-    sinchClient.start(loginObj).then(function() {
-            console.log('\u001b[32mAuthenticated as ' + loginObj.username + '\u001b[39m');
-
-            rl.question("Chat with: ", function(answer) {
-                console.log('===========================');
-                recipientId = answer;
-                rl.setPrompt(loginObj.username + '> ');
-                rl.prompt(true);
-
-                rl.on('line', function(line) {
-                    var message = messageClient.newMessage(recipientId, line);
-
-                    messageClient.send(message).then(function() {
-                        rl.prompt(true);
-                    }).fail(function(e) {
-                        console.log('\u001b[31mError sending message\u001b[39m');
-                        rl.prompt(true);
-                    })
-                })
+    sinchClient.start(loginObj, function() {}, function() {}).then(function() {
+            startChat(loginObj);
+        }).fail(function(e) {
+            console.log('\u001b[31mAuthentication failed \u001b[39m(' + e.message + ')');
+            rl.question('Try create user? ', function(answer) {
+                if (answer.match(/^y(es)?$/i)) {
+                    sinchClient.newUser(loginObj, function(ticket) {
+                        //On success, start the client
+                        sinchClient.start(ticket, function() {
+                            startChat(loginObj);
+                        });
+                    }, function(e) {
+                        console.log('\u001b[31mError creating user \u001b[39m(' + e.message + ')');
+                        quit();
+                    });
+                }
+                else {
+                    quit();
+                }
             });
-        }).fail(function(error) {
-            console.log('\u001b[31m Authentication failed\u001b[39m');
         })
+}
+
+var startChat = function(loginObj) {
+    console.log('\u001b[32mAuthenticated as ' + loginObj.username + '\u001b[39m');
+
+    rl.question("Chat with: ", function(answer) {
+        console.log('===========================');
+        recipientId = answer;
+        rl.setPrompt(loginObj.username + '> ');
+        rl.prompt(true);
+
+        rl.on('line', function(line) {
+            var message = messageClient.newMessage(recipientId, line);
+
+            messageClient.send(message).then(function() {
+                rl.prompt(true);
+            }).fail(function(e) {
+                console.log('\u001b[31mError sending message \u001b[39m(' + e.message + ')');
+                rl.prompt(true);
+            })
+        })
+    });    
+}
+
+var quit = function() {
+    console.log('\nClosing application')
+    sinchClient && sinchClient.terminate();
+    process.kill();
 }
